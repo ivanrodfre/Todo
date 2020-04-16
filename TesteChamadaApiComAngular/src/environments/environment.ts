256556da632a0725c701f5bc1f0af7bbd13957c3
Ivan Rodrigues <ivanrodfre@gmail.com>
2020-04-16 11:57:31 -0300

    Teste chamanda api com Angular

---
+++ TesteChamadaApiComAngular/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDpawTUlBMgGpLLORV7Vp5ldfXh_YL56No",
+    authDomain: "todo-9270a.firebaseapp.com",
+    databaseURL: "https://todo-9270a.firebaseio.com",
+    projectId: "todo-9270a",
+    storageBucket: "todo-9270a.appspot.com",
+    messagingSenderId: "618668945754",
+    appId: "1:618668945754:web:6d7bba8765d4ac2a948363"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
